32f0ca22f3b5aee2a612b1595a9afedc9315289c
whotopia54 <whotopia54@hotmail.fr>
2018-07-21 16:54:44 +0200

    [ADD] Lock system map

---
+++ Assets/Scripts/Scroll_Map.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll_Map : MonoBehaviour {
+	public GameObject Camera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Mouse ScrollWheel") >0F ){
+			if(Camera.transform.position.y <= 200){
+				Camera.transform.position += Vector3.up * 10F;
+			}
+		}
+
+		if (Input.GetAxis ("Mouse ScrollWheel") <0F ){
+			if(Camera.transform.position.y >= 30){
+				Camera.transform.position += Vector3.down * 10F;
+			}
+		}
+
+	}
+}
