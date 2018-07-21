464b3b154d506aded3b239500a517e10c09e2fa5
labroche <corentin.labroche@inist.fr>
2018-07-21 13:21:20 +0200

    [PATCH] close extremity of the map

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -17,15 +17,28 @@ public class Map : MonoBehaviour {
 		for (int z = 0; z < this.wallsVertical.Length; z++) {
 			this.wallsVertical[z] = new GameObject[width];
 			for (int x = 0; x < this.wallsVertical[z].Length; x++) {
-				this.wallsVertical[z][x] = Instantiate (wallInstance, new Vector3 (x * 10 + x, 0, z * 10 + z), new Quaternion (0, 0, 0, 0));
+				if (z != 0) {
+					this.wallsVertical[z][x] = Instantiate (wallInstance, new Vector3 (x * 10 + x, 0, z * 10 + z), new Quaternion (0, 0, 0, 0));
+					Door a = this.wallsVertical[z][x].GetComponentInChildren<Door> ();
+					if (x != 0 && x != this.wallsVertical[z].Length - 1) {
+						a.destroy ();
+					}
+				}
+
 			}
 		}
 		this.wallsHorizontal = new GameObject[height][];
 		for (int z = 0; z < this.wallsHorizontal.Length; z++) {
 			this.wallsHorizontal[z] = new GameObject[width];
 			for (int x = 0; x < this.wallsHorizontal[z].Length; x++) {
-				this.wallsHorizontal[z][x] = Instantiate (wallInstance, new Vector3 (x * 10 + x + 1, 0, z * 10 + z), new Quaternion (0, 0, 0, 0));
-				this.wallsHorizontal[z][x].transform.rotation = Quaternion.Euler (0, 270f, 0);
+				if (x != this.wallsHorizontal[z].Length - 1) {
+					this.wallsHorizontal[z][x] = Instantiate (wallInstance, new Vector3 (x * 10 + x + 1, 0, z * 10 + z), new Quaternion (0, 0, 0, 0));
+					this.wallsHorizontal[z][x].transform.rotation = Quaternion.Euler (0, 270f, 0);
+					if (z != 0 && z != this.wallsHorizontal.Length - 1) {
+						Door a = this.wallsHorizontal[z][x].GetComponentInChildren<Door> ();
+						a.destroy ();
+					}
+				};
 			}
 		}
 	}
