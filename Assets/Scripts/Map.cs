4bc934aa08c69303ced6b6ab17c12ed63d548090
labroche <corentin.labroche@inist.fr>
2018-07-21 22:01:48 +0200

    [REFACTOR]

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -7,12 +7,12 @@ public class Map : MonoBehaviour {
   public GameObject[][] wallsHorizontal;
   public GameObject wallInstance;
   public Player[] players;
-  private int width;
-  private int height;
+  private int width = 0;
+  private int height = 0;
 
   void Start () {
-    this.width = 8;
-    this.height = 8;
+    this.width = this.width != 0 ? this.width + 1 : 5;
+    this.height = this.height != 0 ? this.height + 1 : 5;
 
     this.wallsVertical = new GameObject[height][];
     for (int z = 0; z < this.wallsVertical.Length; z++) {
@@ -20,9 +20,9 @@ public class Map : MonoBehaviour {
       for (int x = 0; x < this.wallsVertical[z].Length; x++) {
         if (z != 0) {
           this.wallsVertical[z][x] = Instantiate (wallInstance, new Vector3 (x * 10 + x, 0, z * 10 + z), new Quaternion (0, 0, 0, 0));
-          Door a = this.wallsVertical[z][x].GetComponentInChildren<Door> ();
           if (x != 0 && x != this.wallsVertical[z].Length - 1) {
-            a.destroy ();
+            Door door = this.wallsVertical[z][x].GetComponentInChildren<Door> ();
+            door.destroy ();
           }
         }
 
@@ -37,8 +37,8 @@ public class Map : MonoBehaviour {
           this.wallsHorizontal[z][x].transform.rotation = Quaternion.Euler (0, 270f, 0);
 
           if (z != 0 && z != this.wallsHorizontal.Length - 1) {
-            Door a = this.wallsHorizontal[z][x].GetComponentInChildren<Door> ();
-            a.destroy ();
+            Door door = this.wallsHorizontal[z][x].GetComponentInChildren<Door> ();
+            door.destroy ();
           }
         };
       }
@@ -61,21 +61,14 @@ public class Map : MonoBehaviour {
           Switch switchComponent = wall.GetComponentInChildren<Switch> ();
           if (switchComponent.player) {
             bool full = true;
-            if (this.wallsHorizontal[z + 1] != null) {
-              if (this.wallsHorizontal[z + 1][x].GetComponentInChildren<Switch> ().player != switchComponent.player) {
-                full = false;
-              }
-
-            }
-            if (this.wallsVertical[z + 1][x + 1]) {
-              if (this.wallsVertical[z + 1][x + 1].GetComponentInChildren<Switch> ().player != switchComponent.player) {
-                full = false;
-              }
-            }
-            if (this.wallsVertical[z + 1][x]) {
-              if (this.wallsVertical[z + 1][x].GetComponentInChildren<Switch> ().player != switchComponent.player) {
-                full = false;
-              }
+            if (
+              z + 1 >= this.wallsHorizontal.Length ||
+              z + 1 >= this.wallsVertical.Length ||
+              this.wallsHorizontal[z + 1][x].GetComponentInChildren<Switch> ().player != switchComponent.player ||
+              this.wallsVertical[z + 1][x + 1].GetComponentInChildren<Switch> ().player != switchComponent.player ||
+              this.wallsVertical[z + 1][x].GetComponentInChildren<Switch> ().player != switchComponent.player
+            ) {
+              full = false;
             }
             if (full) switchComponent.player.score++;
           }
